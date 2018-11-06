cbfa6abb840b21fbba65f5892194f60c8122809e
David Cottrell <cottrell@users.noreply.github.com>
2018-11-06 09:09:13 +0000

    udpate post helper

--- post.sh
+++ post.sh
@@ -2,24 +2,27 @@
 DIR="$( cd "$( dirname $(realpath "${BASH_SOURCE[0]}") )" && pwd )"
 cd $DIR
 # POSTDIR=post/$(whoami)
+# mkdir -p content/$(whoami)
 POSTDIR=post
 mkdir -p content/$POSTDIR
+[[ "$EDITOR" ]] || EDITOR=vim
 if [[ "$1" ]]; then
     filename=$POSTDIR/$1.md
     indexfilename=content/$POSTDIR/$1/index.md
     if [[ -e content/$filename ]]; then
-        vi content/$filename
+        $EDITOR content/$filename
     elif [[ -e  $indexfilename ]]; then
-        vi $indexfilename
+        $EDITOR $indexfilename
     else
         hugo new $filename
+        $EDITOR content/$filename
         git add content/$filename
         git status
     fi
     cd -
 else
     cd content/$POSTDIR
-    vi .
+    $EDITOR .
     cd -
 fi
 cd -
